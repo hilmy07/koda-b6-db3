8ec5d3c4744524e7882df5013562d2b6b5783384
mrrcodex <mrramadhandev@gmail.com>
2026-02-23 11:11:54 +0700

    fix: update num 3 answer

--- query1.sql
+++ query1.sql
@@ -4,7 +4,7 @@ SELECT * FROM movies WHERE "year">2000 ORDER BY "year" ASC;
 SELECT "first_name" FROM actors WHERE "first_name" LIKE '%s' LIMIT 10;
 -- SELECT * FROM actors;
 
-SELECT "id", "name", "year", "rankscore" FROM movies WHERE "rankscore">4.9 AND "rankscore"<7.1 AND "year"=2004 ORDER BY "rankscore" ASC;
+SELECT "id", "name", "year", "rankscore" FROM movies WHERE "rankscore" BETWEEN 5 AND 7 AND "year" BETWEEN 2004 AND 2006 ORDER BY "rankscore" ASC;
 
 SELECT COUNT("rankscore"=6) FROM movies;
 
